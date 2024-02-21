3104cf2bfb7705fa0bea0846149d85a582749c4a
arthur andrade <arthurandrad.23@gmail.com>
2024-02-21 10:35:09 -0300

    String reverser finished

--- Login/src/App.java
+++ Login/src/App.java
@@ -1,21 +1,21 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class App {
-//     public static void main(String[] args) throws Exception {
-//         Scanner read = new Scanner(System.in);  // Create a Scanner object
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);  // Create a Scanner object
 
-//         System.out.println("Enter your name");
-//         String name = read.next();  // Read user input
+        System.out.println("Enter your name");
+        String name = read.next();  // Read user input
 
-//         System.out.println("Enter your password");
-//         String password = read.next();  // Read user input
+        System.out.println("Enter your password");
+        String password = read.next();  // Read user input
 
-//         if (name.equals("Arthur") || password.equals("MGSV") ) {
-//             System.out.printf("Login successful!");
-//         } else {
-//             System.out.printf("Password and/or login incorrect");
-//         }
+        if (name.equals("Arthur") || password.equals("MGSV") ) {
+            System.out.printf("Login successful!");
+        } else {
+            System.out.printf("Password and/or login incorrect");
+        }
         
-//         read.close();
-//     }
-// }
+        read.close();
+    }
+}
---
+++ String Reverser/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Enter your string");
+        String source = read.nextLine();
+        System.out.println(source);
+
+        for (String part : source.split(" ")) {
+            System.out.print(new StringBuilder(part).reverse().toString());
+            System.out.print(" ");
+        }
+        
+        read.close();
+    }
+}
