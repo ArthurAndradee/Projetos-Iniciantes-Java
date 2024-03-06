0f979f0f8a79f6778ae6cbbc57b9796ee5873373
arthur andrade <arthurandrad.23@gmail.com>
2024-03-05 22:39:11 -0300

    Method Overloading done

--- Object-Oriented/Method Overloading/src/Introduction.java
+++ Object-Oriented/Method Overloading/src/Introduction.java
@@ -1,11 +1,11 @@
 public class Introduction {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
         User arthur = new User();
         
         arthur.setName("Arthur"); 
-        arthur.membership = "Gold";
+        arthur.setMembership(User.Membership.Gold);
 
         System.out.println(arthur.getName());
+        System.out.println(arthur._membership);
     }
 }
--- Object-Oriented/Method Overloading/src/User.java
+++ Object-Oriented/Method Overloading/src/User.java
@@ -1,6 +1,6 @@
 public class User {
     private String _name;
-    String membership;
+    String _membership;
 
     void setName(String name) {
         _name = name;
@@ -9,4 +9,23 @@ public class User {
     String getName () {
         return _name;
     }
+
+    void setMembership(String membership) {
+        _membership = membership;
+    }
+
+    void setMembership(Membership membership) {
+        _membership = membership.name();
+    }
+
+    
+    public enum Membership {
+        Bronze, Silver, Gold;
+    }
+
+
+    String getmembership () {
+        return _membership;
+    }
 }
+ 
